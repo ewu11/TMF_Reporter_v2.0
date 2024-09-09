26b91eedb273313d9de09ea99d7bfbd0afa96d1f
Eliasaph Wan Uyo <70519090+ewu11@users.noreply.github.com>
2024-09-10 00:21:10 +0800

    Update streamlit_app.py
    another try -- v1.2xx xberapa menjadi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import re
+from io import StringIO
+import pandas as pd
 
-# Global result dictionary
+# Initialize global result storage with various categories
 global_result = {
     "Full Capping": [],
     "Order Missing/ Pending Processing": [],
@@ -44,42 +46,16 @@ global_result = {
     "Other": []  # This will store both the ticket/ID and the message content
 }
 
-# Define the main function to filter messages
-def filter_messages(file_contents, base_names):
-    timestamp_pattern = re.compile(r'\[\d{2}:\d{2}, \d{1,2}/\d{1,2}/\d{4}\]|^\[\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2} [APM]{2}]')
-    name_patterns = [re.compile(rf'\b{re.escape(name)}\b', re.IGNORECASE) for name in base_names]
-    filtered_lines = []
-    skip_block = False
-    current_message = []
-
-    lines = file_contents.splitlines()
-    for line in lines:
-        if timestamp_pattern.match(line):
-            if current_message:
-                filtered_lines.append(' '.join(current_message).strip().lower())
-                current_message = []
-            if any(pattern.search(line) for pattern in name_patterns):
-                skip_block = True
-            else:
-                skip_block = False
-
-        if not skip_block:
-            current_message.append(line.strip().lower())
-
-    if not skip_block and current_message:
-        filtered_lines.append(' '.join(current_message).strip().lower())
-
-    filtered_text = '\n\n'.join(filtered_lines)
-    return filtered_text
-
 # Function to process the text file input
-def process_messages_from_file(content):
+def process_messages_from_string(content):
     global global_result
     messages = re.split(r'\n(?=\[\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2} (?:am|pm)\])|\[\d{2}:\d{2}, \d{1,2}/\d{1,2}/\d{4}\]', content)
 
+    # Regular expressions for different patterns
     ticket_order_pattern = r'\b1-\d{9,11}\b|\bT-\d{9}\b|\bt-\d{10}\b|\b1-[a-z0-9]{7}\b|\binc\b'
     id_pattern = r'\bQ\d{6}\b|\bq\d{6}\b|\bTM\d{5}\b|\btm\d{5}\b'
-
+    
+    # Issue-specific patterns
     issue_patterns = {
         "Full Capping": r'\bfull cap[p]?ing\b|\bbukan dlm id ui\b|\bcap(p)?ing full\b|\b(tidak|x) as(s)?ign pd ru\b|\bfull slot\b|\btidak boleh ass(i)?gn pada team\b|\bslot id( ni)? x( ?)lepas\b|\bn(a)?k slot id\b|\bfull dalam list\b|\bcapping penuh\b|\bid.*full t(a)?p(i)? d(a)?l(a)?m list tmf.*ada \d order\b|\bui.*(tak|x) n(a)?mp(a)?k (d)?(e)?kat dia\b|\bui kata (x|tak) n(a)?mp(a)?k o(r)?d(e)?r\b|\bbukan ui pnya\b|\bslot balik p(a)?d(a)? (team|ru|ra|ui)\b|\border return(ed)? s(e)?m(a)?l(a)?m.*m(a)?s(i)?h ada d(a)?l(a)?m tm( )?f(orce)?.*ru\b|\bui inf(o)?(r)?m (t(a)?k|x) n(a)?mp(a)?k order\b|\bini order m(e)?m(a)?(n)?g ru p(u)?(n)?ya\b|\b(belum ada|xada|teda|tiada) id mana(2)? ru\b|\b(tidak|tak|x) d(a)?p(a)?t( )?(nak)?assign.*(ru|team)\b|\bord(er)?.*tak( )?d(a)?p(a)?t.*assign p(a)?d(a)? (team|ru)\b|\bbukan order (ui|team)\b|\bid( dah)?( )?full.*d(a)?l(a)?m tm( )?f(orce)?.*hanya ada [1-9] order\b|\b(takleh|xboleh|xleh) slot id\b|\bin( )?hand ui.*assign( ke)? ui\b|\bmasih full/7 order\b|\bin hand.*yg nak assign\b|\bid.*ada \d order t(a)?p(i)? id.*full\b|\bfull.*t(a)?p(i)?.*tm( )?f(orce)? ada \d order\b|\bo(r)?der (d(i)?|p(a)?d(a)?)( id)? ui\b|\bid ni.*(x|tak|tidak)( )?l(e)?p(a)?s.*slot order( lain)?\b|\bd(a)?h full (x|tak)( )?l(e)?p(a)?s slot( order)?\b|\border# ada dlm inhand.*order# nak assign ke ui\b|\btmf saya detect baru \d order\b|\border.*perlu.*masuk.*t(a)?p(i)? (x|tak)( )?(boleh|leh)( masuk)?\b|\bini b(u)?k(a)?n.*ini p(e)?(r)?lu.*masuk(kan)?\b|\btmf.*detect \d order\b|\bfull cappinng\b|\bcapping.*(full|p(e)?n(u)?h)\b',
         "Order Missing/ Pending Processing": r'\b(di|dlm|dalam) (oal|order(?: activity)?(?: list)?)\b|\btmf (?:tak (?:wujud|appear)|x ?appear)\b|\b(di dlm oal|di oal|oal missing|tmf tak wujud|oal record not found|oal not found|oal xfound|oal xappear|oal not appear|oal x appear)\b|\b(?:tiada |masukkan |appear )?(?:order )?(dlm|dalam|in) rol\b|\b(tiada (dalam|dlm)|xda(?: di)?)( scheduled page)\b|\bponr\b|\bpending processing\b|\bmissing( dalam)? oal\b|\b(x?|tak ) masuk( di)?( dlm| dalam)( bakul| basket)\b|\b(?:order\s)?(?:tak\s|tiada\s|xda\s)?(?:masuk\s)?(?:dalam\s)?(?:bakul|basket)\b|\b(tiada|xda|takda) di( dalam)?( page)? activity\b|\btask sync\b|\bpending processing\b|\b(tak|x|tiada)\s*(?:di|dekat|dkt|dalam|dlm)?\s*(scheduled|unscheduled)( page)?\b|\btiada (dlm|dalam) (activity|act|aktivity|actvty) list\b|\b(xtvt|act|activity|actvty) (tak|x) (wujud|wjd)\b|\bmasukkan semula.*rol\b|\bstat(u)?(s)? unshedule(d)?.*(ra|mir|cc)\b|\bstatus( pending)?( )?processing\b|\bo(r)?d(e)?r.*(x|tak)( )?masuk (tmf|tmforce)\b|\b(order )?jadi unschedule(d)?\b|\breschedule(d)?( semula)? ke tm( )?f(orce)?\b|\border x( )?appear( at| di| in)? oal\b|\border ni ada (di)?( )?mana\b|\border return.*status unschedul(e)?(d)?\b|\bb(u)?(t)?t(o)?n return (tiada|xda|xde|takda)\b|\border return(ed)? jadi uns(c)?hedule(d)?\b|\border pending pro(c)?es(s)?ing\b|\border.*hilang.*id ui\b|\border ra.*(tak|x) (m(a)?s(u)?k d(a)?l(a)?m (act(ivity)?)|aktiviti|xtvt) order list\b|\bupdate semula ke rol\b|\btiada|xda d(a)?l(a)?m ro(l|c)\b|\bescalate( )?(ke|m(a)?s(u)?k|d(a)?l(a)?m)?( )?rol\b|\border return j(a)?d(i)? unschedul(e)?(d)?\b|\bb(e)?l(u)?m appear d(a)?l(a)?m act(ivity)? list\b|\border (tidak|x|tak) n(a)?m(p)?(a)?k d(a)?l(a)?m.*d\b|\brecord not found.*slot\b',
@@ -121,45 +97,7 @@ def process_messages_from_file(content):
         "Resource Management Issue": r"\bsalah zone id\b"
     }
 
-    added_tickets = set()
-    added_ids = set()
-
-    for message in messages:
-        found_issue = False
-        for issue, pattern in issue_patterns.items():
-            if re.search(pattern, message, re.IGNORECASE):
-                tickets = re.findall(ticket_order_pattern, message)
-                ids = re.findall(id_pattern, message)
-
-                if issue == "Full Capping":
-                    if ids:
-                        global_result[issue].extend(i for i in ids if i not in added_ids)
-                        added_ids.update(ids)
-                else:
-                    if tickets:
-                        global_result[issue].extend(t for t in tickets if t not in added_tickets)
-                        added_tickets.update(tickets)
-                    if ids:
-                        global_result[issue].extend(i for i in ids if i not in added_ids)
-                        added_ids.update(ids)
-
-                found_issue = True
-                break
-
-        if not found_issue:
-            tickets = re.findall(ticket_order_pattern, message)
-            ids = re.findall(id_pattern, message)
-            if tickets or ids:
-                if tickets:
-                    global_result["Other"].extend([(t, message) for t in tickets if t not in added_tickets])
-                    added_tickets.update(tickets)
-                if ids:
-                    global_result["Other"].extend([(i, message) for i in ids if i not in added_ids])
-                    added_ids.update(ids)
-
-def process_uploaded_files(uploaded_files):
-    global global_result
-    global_result = {
+    result = {
         "Full Capping": [],
         "Order Missing/ Pending Processing": [],
         "Missing Manual Assign Button": [],
@@ -203,48 +141,70 @@ def process_uploaded_files(uploaded_files):
         #basically masuk 'others', due to NO CONTEXT!
     }
 
-    for uploaded_file in uploaded_files:
-        content = uploaded_file.read().decode("utf-8")
-        st.write(f"Processing file: {uploaded_file.name}")  # Debug statement
-        process_messages_from_file(content)
+    added_tickets = set()
+    added_ids = set()
 
-    st.write(f"Global Result: {global_result}")  # Debug statement
-    return global_result
+    for message in messages:
+        found_issue = False
 
-# Streamlit app starts here
-st.title("Message Filter and Categorization App")
+        for issue, pattern in issue_patterns.items():
+            if re.search(pattern, message, re.IGNORECASE):
+                tickets = re.findall(ticket_order_pattern, message)
+                ids = re.findall(id_pattern, message)
 
-# Step 1: Upload the files
-uploaded_files = st.file_uploader("Upload text files", type="txt", accept_multiple_files=True)
-
-# Specify base names for filtering
-base_names = ['Hartina', 'Tina', 'Normah', 'Pom', 'Afizan', 'Pijan', 'Ariff', 'Dheffirdaus', 'Dhef', 'Hazrina', 'Rina', 'Nurul', 'Huda', 'Zazarida', 'Zaza', 'Eliasaph Wan', 'Wan', '] : ', '] :']
-
-# Step 2: Display process button after files are uploaded
-if uploaded_files:
-    if st.button("Filter and Categorize Files"):
-        st.write("Processing files...")
-
-        combined_output = ""
-        for uploaded_file in uploaded_files:
-            file_content = uploaded_file.read().decode("utf-8")
-            filtered_text = filter_messages(file_content, base_names)
-            combined_output += f"Filtered output for {uploaded_file.name}:\n{filtered_text}\n\n"
-
-        result = process_uploaded_files(uploaded_files)
-
-        categorized_output = "Categorized Results:\n"
-        for issue, entries in result.items():
-            if entries:
-                categorized_output += f"\n{issue}:\n"
-                if issue == "Other":
-                    for number, message in entries:
-                        categorized_output += f"{number} - Message: {message}\n"
+                if issue == "Full Capping":
+                    if ids:
+                        result[issue].extend(i for i in ids if i not in added_ids)
+                        added_ids.update(ids)
                 else:
-                    for number in entries:
-                        categorized_output += f"{number}\n"
+                    if tickets:
+                        result[issue].extend(t for t in tickets if t not in added_tickets)
+                        added_tickets.update(tickets)
+                    if ids:
+                        result[issue].extend(i for i in ids if i not in added_ids)
+                        added_ids.update(ids)
 
-        st.text_area("Filtered Content", combined_output, height=300)
-        st.text_area("Categorized Results", categorized_output, height=300)
-else:
-    st.error("Please upload at least one text file.")
+                found_issue = True
+                break
+
+        if not found_issue:
+            tickets = re.findall(ticket_order_pattern, message)
+            ids = re.findall(id_pattern, message)
+            if tickets or ids:
+                if tickets:
+                    result["Other"].extend([(t, message) for t in tickets if t not in added_tickets])
+                    added_tickets.update(tickets)
+                if ids:
+                    result["Other"].extend([(i, message) for i in ids if i not in added_ids])
+                    added_ids.update(ids)
+
+    return result
+
+# Streamlit app
+st.title("Message Processing App")
+
+uploaded_files = st.file_uploader("Choose text files", accept_multiple_files=True, type='txt')
+
+if uploaded_files:
+    # Read and process each uploaded file
+    global_result.clear()
+    for uploaded_file in uploaded_files:
+        content = StringIO(uploaded_file.getvalue().decode('utf-8')).read()
+        result = process_messages_from_string(content)
+
+        # Update global result
+        for key, value in result.items():
+            global_result[key].extend(value)
+
+    # Display results
+    st.subheader("Processing Results")
+
+    for issue, numbers in global_result.items():
+        if numbers:
+            st.write(f"**{issue}:**")
+            if issue == "Other":
+                for number, message in numbers:
+                    st.write(f"{number} - Message: {message}")
+            else:
+                for number in numbers:
+                    st.write(number)
